1b2db2fd39e400a0ebd761436f9b98ad013c94aa
vladimir <m9043887765@gmail.com>
2023-04-22 21:12:38 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import psycopg2
+
+#-----------------------------------------------------создаем таблицу----------------------
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS phone_user(
+            id SERIAL PRIMARY KEY,
+            email_user VARCHAR(40) UNIQUE NOT NULL,
+            phone_user INTEGER 
+        );
+        """)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name TEXT (40) NOT NULL,
+            subname TEXT (40) NOT NULL,
+            email_user VARCHAR(40) UNIQUE NOT NULL
+        );
+        """)
+        conn.commit()  # фиксируем в БД        
+        print(f'[INFO] Table created successfully')
+
+
+#-----------------------------------------------------наполняем таблицу----------------------
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO users(name) VALUES('Spaider') RETURNING id;
+        """)
+        conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO users(subname) VALUES('Man') RETURNING id;
+        """)
+        conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO users(email_esers) VALUES('SpaiderMan@netodology.ru') RETURNING id;
+        """)
+        print(cur.fetchone())  # запрос данных автоматически зафиксирует изменения
+
+
+#-----------------------------------------------------добовляем телефон пользвателю----------
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO phone_users(email_user, phone, users_id) VALUES(SpaiderMan@netodology.ru, '02', 1);
+            """)
+        conn.commit()  # фиксируем в БД
+
+
+#-----------------------------------------------------добовляем данные о клиенте-------------
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO users(name, subname, email_users, users_id) VALUES('Vladimir', 'Putin', 'superman@111111.ru', 1);
+            """)
+        conn.commit()  # фиксируем в БД        
+#-----------------------------------------------------удалить телефон для существующего клиента------
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:        
+        cur.execute("""
+            DELETE FROM phone_user WHERE id=%spaiderman;
+            """, (1,))
+        cur.execute("""
+        SELECT * FROM phone_user;
+        """)
+        print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
+#--------------------------------------------------- удалить существующего клиента.--------------
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+            DELETE FROM user WHERE id=%spaiderman;
+            """, (1,))
+        cur.execute("""
+        SELECT * FROM user;
+        """)
+        print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
+#---------------------------------------------------найти клиента по его данным: имени, фамилии, email или телефону
+with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+         cur.execute("""
+        DELETE FROM users WHERE id=%s;
+        """, (1,))
+        cur.execute("""
+        SELECT * FROM users;
+        """)
+        print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
+
+
+
+
+except Exception as _ex:
+    print('[info] Error while working with PostgreSQL', _ex)
+finally:
+    if connection:
+        connection.close()
+        print('[info] Error while working with PostgreSQL')
