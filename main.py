fa4a4fc6a3c532e49931eded6e3b9880a9c39a9e
vladimir <m9043887765@gmail.com>
2023-04-24 16:31:56 +0500

    j commit

--- main.py
+++ main.py
@@ -2,79 +2,76 @@ import psycopg2
 
 #-----------------------------------------------------создаем таблицу----------------------
 def create_db(conn):
-    conn.cursor() as cur:
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS phone_user(
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS phone_user(
             id SERIAL PRIMARY KEY,
             email_user VARCHAR(40) UNIQUE NOT NULL,
-        phone_user INTEGER)""")
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS users(
+            phone INTEGER)""")
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY,
             name TEXT (40) NOT NULL,
             subname TEXT (40) NOT NULL,
             email_user VARCHAR(40) UNIQUE NOT NULL)""")
-    conn.commit()  # фиксируем в БД        
-    print(f'[INFO] Table created successfully')
-
+        conn.commit()  
 
 #-----------------------------------------------------наполняем таблицу----------------------
-def add_client(conn, name, subname, email, phones=None):
+def add_client(conn, name, subname, email, phone=None):
     with conn.cursor() as cur:
-        cur.execute("""
-        INSERT INTO users(name) VALUES('Spaider') RETURNING id;
-        """)
-        conn.cursor() as cur:
-        cur.execute("""
-        INSERT INTO users(subname) VALUES('Man') RETURNING id;
-        """)
-        conn.cursor() as cur:
-        cur.execute("""
-        INSERT INTO users(email_esers) VALUES('SpaiderMan@netodology.ru') RETURNING id;
-        """)
-        print(cur.fetchone())  # запрос данных автоматически зафиксирует изменения
-
+        cur.execute("""INSERT INTO users(name) VALUES('Spaider') RETURNING id""")
+        cur.execute("""INSERT INTO users(subname) VALUES('Man') RETURNING id""")
+        cur.execute("""INSERT INTO users(email_esers) VALUES('SpaiderMan@netodology.ru') RETURNING id""")
+        print(cur.fetchone())  
 
 #-----------------------------------------------------добовляем телефон пользвателю----------
-with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+def add_phone(conn, users_id, phone_users):
     with conn.cursor() as cur:
         cur.execute("""
             INSERT INTO phone_users(email_user, phone, users_id) VALUES(SpaiderMan@netodology.ru, '02', 1);
             """)
-        conn.commit()  # фиксируем в БД
-
+        conn.commit()  
 
 #-----------------------------------------------------добовляем данные о клиенте-------------
-with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+def change_client(conn, user_id, name=None, subname=None, email_users=None, phones_users=None):
     with conn.cursor() as cur:
         cur.execute("""
-            INSERT INTO users(name, subname, email_users, users_id) VALUES('Vladimir', 'Putin', 'superman@111111.ru', 1);
+            INSERT INTO users(1, name, subname, email_users, phone) VALUES('Vladimir', 'Putin', 'superman@111111.ru', 1);
             """)
-        conn.commit()  # фиксируем в БД        
+        conn.commit()  
+        
 #-----------------------------------------------------удалить телефон для существующего клиента------
-with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+def delete_phone(conn, users_id, phone_users):
     with conn.cursor() as cur:        
         cur.execute("""
-            DELETE FROM phone_user WHERE id=%spaiderman;
+            DELETE FROM phone WHERE id=%spaiderman;
             """, (1,))
         cur.execute("""
         SELECT * FROM phone_user;
         """)
-        print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
+        print(cur.fetchall())  
+
 #--------------------------------------------------- удалить существующего клиента.--------------
-with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+def delete_client(conn, users_id):
     with conn.cursor() as cur:
         cur.execute("""
-            DELETE FROM user WHERE id=%spaiderman;
+            DELETE FROM users WHERE id=%spaiderman;
             """, (1,))
         cur.execute("""
-        SELECT * FROM user;
+        SELECT * FROM users;
         """)
-        print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
+        print(cur.fetchall())  
+
 #---------------------------------------------------найти клиента по его данным: имени, фамилии, email или телефону
-with psycopg2.connect(database="netology_db", user="postgres", password="postgres") as conn:
+def find_client(conn, name=None, subname=None, email_user=None, phone_user=None):
     with conn.cursor() as cur:
-        cur.execute("""DELETE FROM users WHERE id=%s;""", (1,))
-        cur.execute("""SELECT * FROM users""")
-        print(cur.fetchall())  # запрос данных автоматически зафиксирует изменения
-  
+        cur.execute("""
+        SELECT id FROM users WHERE name=%s, subname=%s, emai_user=%s;
+        """, ("Man", "Spaider", "SpaiderMan@netodology.ru" ))  
+        cur.execute("""
+        SELECT id FROM phone_users WHERE phone=%s;
+        """, ("02" ))  
+        print(cur.fetchone())
+
+
+conn.close()
